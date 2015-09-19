c0502ad82fddc6fa6986f676b5aca99720ad4ff7
sherrillmix <sherrillmix@users.noreply.github.com>
2015-09-19 08:23:35 -0400

    Add example plot to R

--- inst/doc/example.R
+++ inst/doc/example.R
@@ -9,3 +9,27 @@ library(DNAPlotR)
 packageKeywords<-"visualization, alignment, display, genome, DNA, sequence, multiple sequence alignment, reference sequence"
 
 
+###################################################
+### code chunk number 2: plotDNA (eval = FALSE)
+###################################################
+##   set.seed(1234)
+##   fakeSeqs<-createFakeDNA(5000,500)
+##   refSeq<-fakeSeqs[1]
+##   fakeSeqs<-fakeSeqs[-1]
+##   species<-sprintf('Species %s',sub(' [0-9]+$','',names(fakeSeqs)))
+##   par(mar=c(3.5,4.4,.5,7),mgp=c(2.5,1,0))
+##   dummy<-plotDNA(fakeSeqs,groups=species)
+
+
+###################################################
+### code chunk number 3: showPlotDNA
+###################################################
+  set.seed(1234)
+  fakeSeqs<-createFakeDNA(5000,500)
+  refSeq<-fakeSeqs[1]
+  fakeSeqs<-fakeSeqs[-1]
+  species<-sprintf('Species %s',sub(' [0-9]+$','',names(fakeSeqs)))
+  par(mar=c(3.5,4.4,.5,7),mgp=c(2.5,1,0))
+  dummy<-plotDNA(fakeSeqs,groups=species)
+
+
